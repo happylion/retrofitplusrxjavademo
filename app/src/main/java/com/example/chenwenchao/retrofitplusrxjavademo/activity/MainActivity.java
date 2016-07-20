48de57d291e11efc559420462f1afb47cb798be3
chenwenchao <chenwenchao@maoyan.com>
2016-07-20 20:27:02 +0800

    格式化代码

--- app/src/main/java/com/example/chenwenchao/retrofitplusrxjavademo/activity/MainActivity.java
+++ app/src/main/java/com/example/chenwenchao/retrofitplusrxjavademo/activity/MainActivity.java
@@ -2,34 +2,23 @@ package com.example.chenwenchao.retrofitplusrxjavademo.activity;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-import com.example.chenwenchao.retrofitplusrxjavademo.R;
 import com.example.chenwenchao.retrofitplusrxjavademo.bean.CityInfoBean;
 import com.example.chenwenchao.retrofitplusrxjavademo.model.CityInfoModel;
-import com.example.chenwenchao.retrofitplusrxjavademo.service.CityInfoService;
 import com.example.chenwenchao.retrofitplusrxjavademo.view.TextButtonTestView;
-
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import retrofit.RxJavaCallAdapterFactory;
-import rx.Subscriber;
-import rx.android.schedulers.AndroidSchedulers;
-import rx.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity implements TextButtonTestView.ITextButtonTestView, CityInfoModel.showCityInfoCallback {
 
     private TextButtonTestView textButtonTestView;
     private CityInfoModel model;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        textButtonTestView=new TextButtonTestView(this,this);
+        textButtonTestView = new TextButtonTestView(this, this);
         setContentView(textButtonTestView.getViewXml());
         textButtonTestView.initView();
-        model=new CityInfoModel(this);
+        model = new CityInfoModel(this);
     }
 
 
